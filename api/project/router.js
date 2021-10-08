9d05e66c74bedb088a5363d6d2d558bcdeca6430
Pauline122112 <pauline.design007@gmail.com>
2021-10-08 18:56:14 -0500

    Completed MVP

--- api/project/router.js
+++ api/project/router.js
@@ -1,25 +1,24 @@
-// build your `/api/projects` router here
-const express = require("express")
-const projectsModel = require('../project/model')
+const express = require("express");
+const projectsModel = require("./model");
 
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
 	projectsModel
 		.getProjects()
 		.then((projects) => {
-			res.status().json(projects);
+			res.status(200).json(projects);
 		})
-		.catch(next);
-})
+		.catch(next)
+});
 
 router.post("/", (req, res, next) => {
 	projectsModel
 		.newProject(req.body)
 		.then((project) => {
-			res.status(201).json(project);
+			res.status(201).json(project)
 		})
-		.catch(next);
-});
+		.catch(next)
+})
 
 module.exports = router
