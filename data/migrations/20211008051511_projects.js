4804dee6ab0d0bd0e65c1fe155cb99d4c5d71edc
Pauline122112 <pauline.design007@gmail.com>
2021-10-08 18:49:44 -0500

    Found the issue in migrations that was preventing the testing from working

--- data/migrations/20211008051511_projects.js
+++ data/migrations/20211008051511_projects.js
@@ -1,37 +1,35 @@
-
 exports.up = async function (knex) {
 	await knex.schema
 		//A **project** is what needs to be done and is stored in a `projects` table
 		.createTable("projects", (table) => {
 			table.increments("project_id");
-			table.string("project_name", 128).notNullable();
-			table.string("project_description", 128).unsigned().notNullable();
+			table.string("project_name",128).notNullable();
+			table.string("project_description");
 			table.boolean("project_completed").defaultTo(false);
 		})
-		//A **resource** is anything needed to complete a project and is stored in a `resources` table
+		//A **resource** is anything needed to complete a project and is stored in ///a `resources` table
 		.createTable("resources", (table) => {
-			table.increments("resource_id");
-			table.string("resource_name").unsigned().notNullable();
-			table.string("resource_description").unsigned().notNullable();
+			table.increments("resource_id")
+			table.string("resource_name").notNullable().unique()
+			table.string("resource_description")
 		})
 		//A **task** is one of the steps needed to complete a project and is stored in a `tasks` table
 		.createTable("tasks", (table) => {
 			table.increments("task_id");
-			table.string("task_description").unsigned().notNullable();
-			table.string("task_notes").unsigned().notNullable();
-			table.integer("task_completed");
-			table
-				.integer("project_id")
+			table.string("task_description").notNullable();
+			table.string("task_notes");
+			table.boolean("task_completed").defaultTo(false);
+			table.integer("project_id")
 				.unsigned()
 				.notNullable()
 				.references("project_id")
 				.inTable("projects")
 				.onDelete("RESTRICT")
-				.onUpdate("RESTRICT");
+				.onUpdate("RESTRICT")
 		})
 		//A **resource assignment** connects a resource and a project, and is stored in a `project_resources` table.
 		.createTable("project_resources", (table) => {
-			table.increments("project_resources_id");
+			table.increments("project_resources_id")
 			table
 				.integer("project_id")
 				.unsigned()
@@ -39,7 +37,7 @@ exports.up = async function (knex) {
 				.references("project_id")
 				.inTable("projects")
 				.onDelete("RESTRICT")
-				.onUpdate("RESTRICT");
+				.onUpdate("RESTRICT")
 			table
 				.integer("resource_id")
 				.unsigned()
@@ -47,16 +45,14 @@ exports.up = async function (knex) {
 				.references("resource_id")
 				.inTable("resources")
 				.onDelete("RESTRICT")
-				.onUpdate("RESTRICT");
+				.onUpdate("RESTRICT")
 		})
 }
-	exports.down = async function (knex) {
-		await knex.schema
-			.dropTableIfExists("projects")
-			.dropTableIfExists("resources")
-			.dropTableIfExists("tasks")
-			.dropTableIfExists("project_resources")
-	}
 
-
-	
+exports.down = async function (knex) {
+	await knex.schema
+		.dropTableIfExists("project_resources")
+		.dropTableIfExists("tasks")
+		.dropTableIfExists("resources")
+		.dropTableIfExists("projects")
+}
