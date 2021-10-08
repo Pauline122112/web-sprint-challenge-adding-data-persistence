8ab5531be282a5b45c1334f024d2c1d5bb0b5e69
Pauline122112 <pauline.design007@gmail.com>
2021-10-08 13:30:07 -0500

    refactored migration table and working on projects model.js

--- api/project/model.js
+++ api/project/model.js
@@ -1,9 +1,19 @@
 // build your `Project` model here
 const db = require("../data/db-config");
 
-const getProjects = () => {
-	return db("projects");
-};
+const getProjects = async () => {
+	const allProjects = await db('projects')
+	const projects = allProjects.map((project) => {
+		const projectTest = {
+			project_id: project.project_id,
+			project_name: project.project_name,
+			project_description: project.project_description,
+			project_completed: project.project_completed === 0 ? false : true,
+		}
+		return projectTest
+	})
+	return projects
+}
 
 const getProjectById = (id) => {
 	return db("projects").where({ id }).first();
