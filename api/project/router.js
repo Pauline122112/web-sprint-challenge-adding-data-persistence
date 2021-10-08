96e3df002a03a72f2136cfd021f1cbd9f89a4962
Pauline122112 <pauline.design007@gmail.com>
2021-10-08 00:26:56 -0500

    working on router.js for projects folder

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,28 @@
 // build your `/api/projects` router here
+const express = require("express")
+
+const Project = require('../project/model')
+
+const router = express.Router()
+
+router.get("/", (_, res) => {
+	Project.getProjects()
+		.then((recipes) => {
+			res.status(200).json(recipes)
+		})
+		.catch((error) => {
+			res.status(500).json({ message: error.message })
+		})
+})
+
+router.get("/:id", (req, res) => {
+	try {
+		Project.getProjectById(req.params.id).then((recipe) => {
+			res.status(200).json(recipe)
+		})
+	} catch (error) {
+		res.status(500).json({ message: error.message })
+	}
+})
+
+module.exports = router
